ebccaca2b95f6c0f4177201e6045aa95315c2645
utkarsha.singh <t-utsing@microsoft.com>
2017-06-21 15:38:10 +0800

    Adding dialog

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 //fdsffsfds
 var restify = require('restify');
 var builder = require('botbuilder');
-
+var rp = require('request-promise');
+var BINGSEARCHKEY = 'b0ddc0ab720d4ebe827b85a29aaab14a';
 //=========================================================
 // Bot Setup
 //=========================================================
@@ -62,15 +63,91 @@ intentDialog.matches(/\b(hi|hello|hey|howdy|what's up|anyone there|wassup|yo)\b/
     .matches('ExpiredSession', '/inactive') //Check for LUIS intent to answer how it affects 
     .matches('CannotAccess', '/cannot') //Check for LUIS intent to answer how it affects
     .matches('SupportedDevices', '/support') //Check for LUIS intent to answer how it affects 
-    .onDefault(builder.DialogAction.send("Sorry, I didn't understand what you said.")); //Default message if all checks fail
+    .matches('GetNews', '/topNews') //Check for LUIS intent to get news
+    .onDefault(builder.DialogAction.send("Oops! Can't answer that one. However, in addition to Payroll details, I can still get you some News!")); //Default message if all checks fail
 
-bot.dialog('/print', function (session) {
-    session.send('Yes, MyPayslips can be printed in the same way that you would print any other document when using a pdf viewer. Simply select the print icon in the pdf viewer, which will appear above ');
+bot.dialog('/topNews', [
+    function (session) {
+        // Ask the user which category they would like
+        // Choices are separated by |
+        builder.Prompts.choice(session, "Which category would you like?", "Technology|Science|Sports|Business|Entertainment|Politics|Health|World|(quit)");
+    }, function (session, results, next) {
+        // The user chose a category
+        if (results.response && results.response.entity !== '(quit)') {
+            //Show user that we're processing their request by sending the typing indicator
+            session.sendTyping();
+            // Build the url we'll be calling to get top news
+            var url = "https://api.cognitive.microsoft.com/bing/v5.0/news/?"
+                + "category=" + results.response.entity + "&count=10&mkt=en-US&originalImg=true";
+            // Build options for the request
+            var options = {
+                uri: url,
+                headers: {
+                    'Ocp-Apim-Subscription-Key': BINGSEARCHKEY
+                },
+                json: true // Returns the response in json
+            }
+            //Make the call
+            rp(options).then(function (body) {
+                // The request is successful
+                sendTopNews(session, results, body);
+            }).catch(function (err) {
+                // An error occurred and the request failed
+                console.log(err.message);
+                session.send("Argh, something went wrong. :( Try again?");
+            }).finally(function () {
+                // This is executed at the end, regardless of whether the request is successful or not
+                session.endDialog();
+            });
+        }
+        else {
+            // The user choses to quit
+            session.endDialog("Ok. Mission Aborted.");
+        }
+
+    }
+]);
+
+
+// This function processes the results from the API call to category news and sends it as cards
+function sendTopNews(session, results, body) {
+    session.send("Top news in " + results.response.entity + ": ");
+    //Show user that we're processing by sending the typing indicator
+    session.sendTyping();
+    // The value property in body contains an array of all the returned articles
+    var allArticles = body.value;
+    var cards = [];
+    // Iterate through all 10 articles returned by the API
+    for (var i = 0; i < 10; i++) {
+        var article = allArticles[i];
+        // Create a card for the article and add it to the list of cards we want to send
+        cards.push(new builder.HeroCard(session)
+            .title(article.name)
+            .subtitle(article.datePublished)
+            .images([
+                //handle if thumbnail is empty
+                builder.CardImage.create(session, article.image.contentUrl)
+            ])
+            .buttons([
+                // Pressing this button opens a url to the actual article
+                builder.CardAction.openUrl(session, article.url, "Full article")
+            ]));
+    }
+    var msg = new builder.Message(session)
+        .textFormat(builder.TextFormat.xml)
+        .attachmentLayout(builder.AttachmentLayout.carousel)
+        .attachments(cards);
+    session.send(msg);
+}
+
+
+bot.dialog('/sayHi', function (session) {
+    session.send('Hi there! I am your Payroll Assistant! Try saying things like "What is MyPayslip?"');
     session.endDialog();
 });
 
-bot.dialog('/sayHi', function (session) {
-    session.send('Hi there! Try saying things like "What is MyPayslip?"');
+bot.dialog('/print', function (session) {
+    session.send('Yes, MyPayslips can be printed in the same way that you would print any other document when using a pdf viewer. Simply select the print icon in the pdf viewer, which will appear above ');
     session.endDialog();
 });
 
